3af1eced454f78e59cc4f450fdb247287b369274
Mark Teekman <markteekman@users.noreply.github.com>
2021-03-22 09:43:30 +0100

    Create check-if-instances-excist.js

---
+++ javascript/check-if-instances-excist.js
@@ -0,0 +1,9 @@
+// select multiple instances
+const instances = document.querySelectorAll('.instance')
+
+// check if instances excist before executing code
+if (instances.length > 0) {
+  instances.forEach(instance => {
+    // ...
+  })
+}
