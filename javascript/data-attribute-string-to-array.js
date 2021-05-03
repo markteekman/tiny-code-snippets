a5c96b544d339fc5ac73dcf39cecce65b22a0f4a
Mark Teekman <markteekman@users.noreply.github.com>
2021-05-03 09:20:36 +0200

    Update data-attribute-string-to-array.js

--- javascript/data-attribute-string-to-array.js
+++ javascript/data-attribute-string-to-array.js
@@ -4,7 +4,10 @@ const element = document.querySelector('[data-my-attribute]')
 const elementAttribute = element.getAttribute('data-my-attribute')
 
 const attributeArray = elementAttribute
-  .replace('[', '') // remove bracket
-  .replace(']', '') // remove bracket
-  .split(',') // split multiple values
+  .replace('[', '')   // remove bracket
+  .replace(']', '')   // remove bracket
+  .split(',')         // split multiple values
   .map(l => l.trim()) // trim whitespace
+
+// Result
+console.log(attributeArray) // ["one", "two"]
