28d255075389e42be39dd1522e70dbf3939ca8b4
Mark Teekman <markteekman@users.noreply.github.com>
2021-07-02 11:08:32 +0200

    Add single file component starter in vue folder

---
+++ vue/single-file-component-starter.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>{{ text }}</p>
+</template>
+
+<script>
+  export default {
+    name: ComponentName,
+    props: {
+      text: {
+        type: String
+      }
+    }
+  }
+</script>
+
+<style>
+  p {
+    font-size: 1em;
+    line-height: 1.1em;
+  }
+</style>
