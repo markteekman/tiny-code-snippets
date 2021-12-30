262feda18226ff3bc624fe0b01b722587c65b5a4
Mark Teekman <markteekman@users.noreply.github.com>
2021-12-30 16:58:10 +0100

    Create vue-router-transitions.vue

---
+++ vue/vue-router-transitions.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <router-view v-slot="slotProps">
+      <transition name="route" mode="out-in">
+        <component :is="slotProps.Component" />
+      </transition>
+    </router-view>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'ComponentName'
+}
+</script>
+
+<style scoped>
+.route-enter-active {
+  opacity: 0;
+  transform: translateY(-10px);
+  transition: all 0.3s ease-in-out;
+}
+
+.route-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
